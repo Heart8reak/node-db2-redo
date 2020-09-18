b819c5d88c4d74c98079417331fb7d62fc1c39f4
Carlos Baez <c.baez1223@yahoo.com>
2020-09-18 00:32:00 -0400

    correct typeError in the carsRouter

--- router/carsRouter.js
+++ router/carsRouter.js
@@ -25,14 +25,13 @@ router.post('/', (req, res) => {
 //##############################
 
 router.get('/', (req, res) => {
-	db('cars').then(carsData => {
-		res
-			.status(200)
-			.json({ message: 'successfully retrieved your data', carsData })
-			.catch(err => {
-				res.status(500).json({ message: 'Failed to retrieve yoru data', err });
-			});
-	});
+	db('cars')
+		.then(carsData => {
+			res.status(200).json({ message: 'successfully retrieved your data', carsData });
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Failed to retrieve your data', err });
+		});
 });
 
 //##############################
